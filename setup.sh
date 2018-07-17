e7da8ccfd9da1b49f11217b65f3b09910e326f24
Philip Rodrigues <rodriges@fnal.gov>
2018-07-17 16:11:59 +0100

    Add setup.sh file to set up the necessary external packages at Ox and FNAL

---
+++ setup.sh
@@ -0,0 +1,12 @@
+source /cvmfs/fermilab.opensciencegrid.org/products/larsoft/setups
+setup gcc v6_4_0
+swdir_fnal=/dune/app/users/rodriges/sw
+swdir_ox=/home/rodrigues/dune/daq/primsim/external
+swdir=""
+[ -e "$swdir_fnal" ] && swdir=$swdir_fnal
+[ -e "$swdir_ox" ] && swdir=$swdir_ox
+[ -z "$swdir" ] && echo Found no external software directory
+prepend LD_LIBRARY_PATH $swdir/lib
+prepend PATH $swdir/bin
+prepend MANPATH $swdir/share/man
+unset swdir swdir_fnal swdir_ox
